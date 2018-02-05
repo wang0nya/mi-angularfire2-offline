71179a486ec1a6ef9e675f4a3e34f7b507608e73
wang0nya <kwangonya@gmail.com>
2018-02-05 07:43:46 +0300

    add highchart, other edits

--- src/pages/reports/reports.ts
+++ src/pages/reports/reports.ts
@@ -17,9 +17,6 @@ import firebase from 'firebase';
 export class ReportsPage {
   userId: any;
   public sales: AfoListObservable<any[]>;
-  public productList:Array<any>;
-  public loadedProductList:Array<any>;
-  public productRef:firebase.database.Reference;
 
     constructor(public navCtrl: NavController,private afoDatabase: AngularFireOfflineDatabase,
       public alertCtrl: AlertController, public afAuth: AngularFireAuth,
@@ -27,53 +24,7 @@ export class ReportsPage {
         afAuth.authState.subscribe( user => {
       if (user) { this.userId = user.uid }
       this.sales = afoDatabase.list(`/userProfile/${this.userId}/sales`);
-
-      // searchbar
-      this.productRef = firebase.database().ref(`/userProfile/${this.userId}/sales`);
-
-      this.productRef.orderByChild("gr").equalTo("true").on('value', productList => {
-        let products = [];
-        productList.forEach( product => {
-          products.push(product.val());
-          return false;
-        });
-
-        this.productList = products;
-        this.loadedProductList = products;
-      });
-      // searchbar end
-
     });
     }
-    initializeItems(){
-      this.productList = this.loadedProductList;
-    }
-    getItems(searchbar) {
-      // Reset items back to all of the items
-      this.initializeItems();
 
-      // set q to the value of the searchbar
-      var q = searchbar.srcElement.value;
-
-
-      // if the value is an empty string don't filter the items
-      if (!q) {
-        return;
-      }
-
-      this.productList = this.productList.filter((v) => {
-        if(v.grn && q) {
-          if (v.grn.toLowerCase().indexOf(q.toLowerCase()) > -1) {
-            return true;
-          }
-          return false;
-        }
-      });
-
-      console.log(q, this.productList.length);
-
-    }
-    removeProduct(key: string) {
-      this.sales.remove(key);
-    }
 }
