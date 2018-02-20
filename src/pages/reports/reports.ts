bba08dc2703d4acbec9bf3c26722a5d5e1683f24
wang0nya <kwangonya@gmail.com>
2018-02-20 09:57:47 +0300

    report test csv

--- src/pages/reports/reports.ts
+++ src/pages/reports/reports.ts
@@ -6,85 +6,62 @@ import {
 
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
+import * as papa from 'papaparse';
 
 @Component({
   selector: 'page-reports',
   templateUrl: 'reports.html'
 })
 export class ReportsPage {
-  userId: any;
-sales = [];
+userId: any;
+sales: any;
 public salesRef:firebase.database.Reference;
-public lineChartData:Array<any> = [
-  {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-  {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-  {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-];
-public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-public lineChartOptions:any = {
-  responsive: true
-};
-public lineChartColors:Array<any> = [
-  { // grey
-    backgroundColor: 'rgba(148,159,177,0.2)',
-    borderColor: 'rgba(148,159,177,1)',
-    pointBackgroundColor: 'rgba(148,159,177,1)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-  },
-  { // dark grey
-    backgroundColor: 'rgba(77,83,96,0.2)',
-    borderColor: 'rgba(77,83,96,1)',
-    pointBackgroundColor: 'rgba(77,83,96,1)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgba(77,83,96,1)'
-  },
-  { // grey
-    backgroundColor: 'rgba(148,159,177,0.2)',
-    borderColor: 'rgba(148,159,177,1)',
-    pointBackgroundColor: 'rgba(148,159,177,1)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-  }
-];
-public lineChartLegend:boolean = true;
-public lineChartType:string = 'line';
+headerRow: any;
 
-// public randomize():void {
-//   let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-//   for (let i = 0; i < this.lineChartData.length; i++) {
-//     _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-//     for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-//       _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-//     }
-//   }
-//   this.lineChartData = _lineChartData;
-// }
-
-// events
-public chartClicked(e:any):void {
-  console.log(e);
-}
-
-public chartHovered(e:any):void {
-  console.log(e);
-}
     constructor(public navCtrl: NavController,private afoDatabase: AngularFireOfflineDatabase,
       public alertCtrl: AlertController, public afAuth: AngularFireAuth,
     public actionSheetCtrl: ActionSheetController) {
         afAuth.authState.subscribe( user => {
       if (user) { this.userId = user.uid }
-      this.salesRef = firebase.database().ref(`/userProfile/${this.userId}/sales`);
+      this.salesRef = firebase.database().ref(`/userProfile/${this.userId}/purchases`);
       this.salesRef.on('value', resp => {
           this.sales = [];
           this.sales = snapshotToArray(resp);
         });
     });
     }
+    private extractData(res) {
+       let sales = res['_body'] || '';
+       let unparsedData = papa.unparse(sales);
 
+       this.headerRow = unparsedData[0];
+
+       // unparsedData.splice(0, 1);
+       this.sales = unparsedData;
+ }
+
+    downloadCSV() {
+        let csv = papa.unparse({
+          fields: this.headerRow,
+          data: this.sales
+        });
+        // Dummy implementation for Desktop download purpose
+        var blob = new Blob([csv]);
+        var a = window.document.createElement("a");
+        a.href = window.URL.createObjectURL(blob);
+        a.download = "report.csv";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      }
+
+      private handleError(err) {
+      console.log('something went wrong: ', err);
+    }
+
+      trackByFn(index: any, item: any) {
+      return index;
+    }
 }
 export const snapshotToArray = snapshot => {
     let returnArr = [];
