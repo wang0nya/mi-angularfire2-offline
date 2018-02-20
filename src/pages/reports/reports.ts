746f28f55457ac9bbc90fd5d2df80d589a8621f5
wang0nya <kwangonya@gmail.com>
2018-02-20 16:36:19 +0300

    dummy chart

--- src/pages/reports/reports.ts
+++ src/pages/reports/reports.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, AlertController,
   ActionSheetController } from 'ionic-angular';
 import {
@@ -9,12 +9,15 @@ import * as firebase from 'firebase';
 import * as papa from 'papaparse';
 import { File } from '@ionic-native/file';
 import { Toast } from '@ionic-native/toast';
-
+import { Chart } from 'chart.js';
 @Component({
   selector: 'page-reports',
   templateUrl: 'reports.html'
 })
 export class ReportsPage {
+  @ViewChild('barCanvas') barCanvas;
+
+    barChart: any;
 userId: any;
 stocks: any;
 transactions: any;
@@ -122,6 +125,47 @@ headerRow: any;
       trackByFn(index: any, item: any) {
       return index;
     }
+
+    ionViewDidLoad() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Sales',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+
+    });
+  }
 }
 export const snapshotToArray = snapshot => {
     let returnArr = [];
