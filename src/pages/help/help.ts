3ae6250ec9b7d5bfa38cb81868529b512f0e286d
wang0nya <kwangonya@gmail.com>
2018-02-25 18:21:56 +0300

    add help page

--- src/pages/help/help.ts
+++ src/pages/help/help.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-help',
@@ -9,5 +10,8 @@ export class HelpPage {
 
   constructor(public navCtrl: NavController) {
   }
-  
+  goToHome(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(HomePage);
+  }
 }
